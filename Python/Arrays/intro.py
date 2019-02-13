d1fb4e2fd585060eab2eb0e6c6cc032855829067
Mark Cerritelli <cerritelli@Marks-MacBook-Pro.local>
2019-02-12 23:36:37 -0500

    Add some basic things to intro

---
+++ Python/Arrays/intro.py
@@ -0,0 +1,34 @@
+# Part 1
+# Add "Puppy" as the 3rd element in the following array.
+
+animal_list = ["Horse", "Pony", "Cat", "Monkey", "Zebra", "Elephant", "Human"]
+
+
+
+# Part 2
+# Create a new list that is a concatination of the first and second lists.
+
+first_list = ["Horse", "Pony", "Cat"]
+
+second_list = ["Monkey", "Zebra", "Elephant"]
+
+
+
+
+# Part 3
+# Create a new list that contains each element of the two lists interleved
+# Horse, Monkey, Pony, Zebra, Cat, Elephant
+
+first_list = ["Horse", "Pony", "Cat"]
+
+second_list = ["Monkey", "Zebra", "Elephant"]
+
+
+
+# Part 4
+# In the first list, insert the elements of the second list, interleved.
+# Horse, Monkey, Pony, Zebra, Cat, Elephant
+
+first_list = ["Horse", "Pony", "Cat"]
+
+second_list = ["Monkey", "Zebra", "Elephant"]
