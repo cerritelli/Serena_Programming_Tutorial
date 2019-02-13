fe2c6af41c8fdd0774fc9084fbf4483e76054ec1
Mark Cerritelli <cerritelli@Marks-MacBook-Pro.local>
2019-02-13 00:04:18 -0500

    Some basic C code

---
+++ C/Basics/README.md
@@ -0,0 +1,23 @@
+To run print_some_bananas.c, you first have to compile it.
+
+`gcc print_some_bananas.c`
+
+this will create a file called a.out
+
+This is your compiled code.
+
+To run the program, you then need to run this compiled code
+
+`./a.out`
+
+You can rename this to whatever you want.
+
+`mv a.out banana`
+
+`./banana`
+
+You can also compile it to a specific name
+
+`gcc print_some_bananas.c -o Banana`
+
+`./Banana`
---
+++ C/Basics/print_some_bananas.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(){
+  
+  // You have to declare the type of each variable.
+  // So total is of type int.  And can only store integers.
+  int total = 0;
+
+  // banana is of type char*. Which is basically what you would use
+  // as the type for Strings.  Strings are arrays of char (character).
+  // We'll explore chars and strings in more detail later.
+  char *banana = "Banana";
+
+  // This is the basic structure of a for loop.
+  for(int i = 0; i < 10; i++) {
+    total += i;
+
+    // This is the formated print function.  It will output
+    // The string, and replace the %d with whatever digit you put later.
+    // We'll explore this in more detials later.
+    printf("%d More banana!\n", i);
+  }
+  
+  // With printf, if you don't end with a \n, it doesn't add a new line character
+  // Which means any further prints will be on the same line
+  printf("%d bananas...", total);
+  printf("That is a lot of bananas!\n");
+}
